b8cc9102b6f34ed8b5a3b0956cd3f6bcb2315cbc
FishyFoshy <157636316+FishyFoshy@users.noreply.github.com>
2025-03-28 00:04:06 -0400

    building init

--- App/controllers/initialize.py
+++ App/controllers/initialize.py
@@ -7,4 +7,5 @@ def initialize():
     db.drop_all()
     db.create_all()
     create_user('bob', 'bobpass')
-    create_building("test")
+    building = create_building("test")
+    building.addDrawing('{"type": "Feature", "properties": {}, "geometry": {"type": "Polygon", "coordinates": [[[-61.400468, 10.642405], [-61.400468, 10.644958], [-61.396264, 10.644958], [-61.396264, 10.642405], [-61.400468, 10.642405]]]}}')
